5a5db246939f21dd8c0ed65a57b4f07602e305df
Wouze <m-ohm1@hotmail.com>
2023-05-14 19:05:30 +0300

    first commit

---
+++ JavaProject.java
@@ -0,0 +1,17 @@
+
+package javaproject;
+
+/**
+ *
+ * @author m-ohm
+ */
+public class JavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
